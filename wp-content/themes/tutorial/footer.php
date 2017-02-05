2fbe2873d9a9662ac7e8c55f2a44a176630dc9af
yaoyang <yaoyang@mobvoi.com>
2017-02-05 13:59:49 +0800

    page

---
+++ wp-content/themes/tutorial/footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+<p>
+Copyright © 2007 <?php bloginfo(’name’); ?>
+</p>
+</div>
+
+</div>
+</body>
+
+
+</html>
--- wp-content/themes/tutorial/search.php
+++ wp-content/themes/tutorial/search.php
@@ -9,7 +9,7 @@
 
 		<div class="entry">
 
-			<?php the_content(); ?>
+			<?php the_excerpt(); ?>
 						<p class="postmetadata">
 <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
 <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
