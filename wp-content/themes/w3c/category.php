8636d6280bc8c242a1e9284372c32173a0db809e
ejoful <ejoful@gmail.com>
2017-02-07 00:03:42 +0800

    add home category

--- wp-content/themes/w3c/category.php
+++ wp-content/themes/w3c/category.php
@@ -13,39 +13,27 @@
 
 get_header(); ?>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
-
-		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
-
-			<?php if ( category_description() ) : // Show an optional category description ?>
-				<div class="archive-meta"><?php echo category_description(); ?></div>
-			<?php endif; ?>
-			</header><!-- .archive-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/* Include the post format-specific template for the content. If you want to
-				 * this in a child theme then include a file called called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-
-			endwhile;
-
-			twentytwelve_content_nav( 'nav-below' );
-			?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
-		</div><!-- #content -->
-	</section><!-- #primary -->
+<?php 
+if ( is_category() ) : 
+global $post; 
+$categories = get_the_category(); 
+foreach ($categories as $category) : 
+?> 
+<div class='widget widget_pages'> 
+<h3>本分类最近更新</h3> 
+<ul> 
+<?php 
+$posts = get_posts('numberposts=1&category='. $category->term_id); 
+foreach($posts as $post) : 
+?> 
+<li> 
+<a href='<?php the_permalink(); ?>'><?php echo mb_strimwidth(get_the_title(), 0, 40, '…'); ?></a> 
+</li> 
+<?php endforeach; ?> 
+</ul> 
+</div> 
+<?php 
+endforeach; endif ; ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
